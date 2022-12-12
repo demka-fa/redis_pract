188c02a432013cf249be339208dd083e5679da6f
Demka <16972045+GeorgiyDemo@users.noreply.github.com>
2022-12-12 23:37:05 +0300

    Pract 2 - all CRUD operations example added

--- pract2/example_simple.py
+++ pract2/example_simple.py
@@ -1,4 +1,4 @@
-#pip3 install redis
+# pip3 install redis
 import random
 
 import redis
@@ -7,32 +7,42 @@ CURRENT_DB = 0
 CURRENT_HOST = "127.0.0.1"
 CURRENT_PASSWORD = "password"
 
-def main():
 
+def main():
     cli = redis.Redis(host=CURRENT_HOST, password=CURRENT_PASSWORD, port=6379, decode_responses=True, db=CURRENT_DB)
 
-    #Пример сохранения int
+    # Пример сохранения int
     cli.set("example_int", 1)
     print(cli.get("example_int"))
 
-    #Пример сохранения float
+    # Пример сохранения float
     cli.set("example_float", 1.1)
     print(cli.get("example_float"))
 
-    #Пример сохранения str
+    # Пример сохранения str
     cli.set("example_str", "str")
     print(cli.get("example_str"))
 
-    #Пример сохранения bytes
+    # Пример сохранения bytes
     cli.set("example_bytes", b"bytes_example")
     print(cli.get("example_bytes"))
 
-    #Пример получения всех данных + их удаление
+    # Пример получения всех данных + их удаление
     for key in cli.keys("example_*"):
         print(f"Key -> {key}, value -> {cli.get(key)} ")
         cli.delete(key)
 
+    # Убедимся в том, что
     print(len(cli.keys("example_*")))
+
+    # Пример обновления данных
+    cli.set("example_int", 41)
+    print(cli.get("example_int"))
+    cli.set("example_int", 42)
+    print(cli.get("example_int"))
+    cli.incr("example_int")
+    print(cli.get("example_int"))
+
 
 if __name__ == "__main__":
     main()
