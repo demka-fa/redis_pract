8505d14c0642b82a4732e4c1c222d8d7a8b929ff
Demka <16972045+GeorgiyDemo@users.noreply.github.com>
2022-12-12 16:33:48 +0300

    Example 1 added

---
+++ pract1/exmaple.py
@@ -0,0 +1,24 @@
+#pip3 indtall redis
+import redis
+
+#TODO Поменять
+#CURRENT_DB = 1
+#CURRENT_HOST = "127.0.0.1"
+#CURRENT_PASSWRD = "password"
+
+CURRENT_DB = 7
+CURRENT_HOST = "127.0.0.1"
+CURRENT_PASSWRD = "password"
+
+def main():
+
+    cli = redis.Redis(host=CURRENT_HOST, password=CURRENT_PASSWRD, port=6379, decode_responses=True, db=CURRENT_DB)
+
+    current_key = "fa"
+    
+    print(cli.get(current_key))
+    cli.set(current_key, "value")
+    print(cli.get(current_key))
+
+if __name__ == "__main__":
+    main()
